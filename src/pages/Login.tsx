2c49a9e16302fe7385207b0a926cd3b208f3ef7b
Rabby <mdrabbybd4747@gmail.com>
2024-07-04 22:08:58 +0600

    create authApi with redux, basic login page and decoded json web token

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "antd"
+import { useForm } from "react-hook-form"
+import authApi from "../redux/features/auth/authApi"
+import { useAppDispatch } from "../redux/hooks"
+import { setUser } from "../redux/features/auth/authSlice"
+import { verifyToken } from "../utils/verifyToken"
+
 const Login = () => {
-	return <div></div>
+	const dispatch = useAppDispatch()
+
+	const { register, handleSubmit } = useForm({
+		defaultValues: {
+			userId: "A-0001",
+			password: "rabby123",
+		},
+	})
+
+	const [login, { error }] = authApi.useLoginMutation()
+
+	const onSubmit = async (data: any) => {
+		const userInfo = {
+			id: data.userId,
+			password: data.password,
+		}
+		const res = await login(userInfo).unwrap()
+		const user = verifyToken(res.data.accessToken)
+
+		dispatch(setUser({ user: user, token: res.data.accessToken }))
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div>
+					<label htmlFor="id">ID</label>
+					<input type="text" id="id" {...register("userId")} />
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input type="text" id="password" {...register("password")} />
+				</div>
+				<Button htmlType="submit">Login</Button>
+			</form>
+		</div>
+	)
 }
 
 export default Login
